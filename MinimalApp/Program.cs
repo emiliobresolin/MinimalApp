f77157629d8ec4df129a183cd07fc0028345727a
emiliobresolin <emiliotdm@hotmail.com>
2023-03-21 10:48:32 -0300

    setup create method in program dot CS

--- MinimalApp/Program.cs
+++ MinimalApp/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using MinimalApp.Models;
+using MinimalApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ICoffeeService, CoffeeService>(); // this is about register the product service with the product interface
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/create", (CoffeeModel coffee, ICoffeeService service) => 
+    {
+        var result = service.Create(coffee);
+        return Results.Ok(result); //this is how we call the create method.
+    });
+
 
 app.Run();
