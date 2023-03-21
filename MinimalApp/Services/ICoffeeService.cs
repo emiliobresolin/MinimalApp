236c9fed3c6ef51dcc7178c2db014924dd1cd568
emiliobresolin <emiliotdm@hotmail.com>
2023-03-21 10:28:32 -0300

    adding interface to services folder

---
+++ MinimalApp/Services/ICoffeeService.cs
@@ -0,0 +1,13 @@
+﻿using MinimalApp.Models;
+
+namespace MinimalApp.Services
+{
+    public interface ICoffeeService
+    {
+        public CoffeeModel Create(CoffeeModel coffee); //this is about the coffee that is about to be created
+        public CoffeeModel? Get(int id); //this is about returning the coffee by its ID
+        public List<CoffeeModel> List(); //this is about returning a list of coffees
+        public CoffeeModel? Update(CoffeeModel coffee); //this is about changing a coffee model
+        public bool Delete(int id); //this is about delete a coffee model
+    }
+}
