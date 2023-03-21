196cd14a2dede56982305c200c9554594b3ee45d
emiliobresolin <emiliotdm@hotmail.com>
2023-03-21 10:18:11 -0300

    adding models to app

---
+++ MinimalApp/Models/CoffeeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApp.Models
+{
+    public class CoffeeModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
