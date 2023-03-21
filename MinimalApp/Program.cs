a92fbe62cc9a8e47ad480350c8fb89272911dda5
emiliobresolin <emiliotdm@hotmail.com>
2023-03-21 11:10:12 -0300

    setup LIST method in program dot CS

--- MinimalApp/Program.cs
+++ MinimalApp/Program.cs
@@ -13,11 +13,20 @@ app.MapPost("/create", (CoffeeModel coffee, ICoffeeService service) =>
         var result = service.Create(coffee);
         return Results.Ok(result); //this is how we call the create method.
     });
+
+
 app.MapGet("/get", (int id, ICoffeeService service) =>
 {
     var coffee = service.Get(id);
     if (coffee is null) return Results.NotFound("Coffee not found");
     return Results.Ok(coffee); //this is how we call the get method.
+});
+
+app.MapGet("/list", (ICoffeeService service) =>
+{
+    var coffee = service.List();
+    
+    return Results.Ok(coffee); //this is how we call the list method.
 });
 
 app.Run();
